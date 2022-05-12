205f99bfa8e41a8c98a6cfc4484b8b8fbe861f32
artem712 <artemlarch77@gmail.com>
2022-05-11 19:51:56 -0700

    change database settings

--- config/database.js
+++ config/database.js
@@ -1,11 +1,36 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      connection: {
+        client: "postgres",
+        connection: {
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "strapi"),
+          user: env("DATABASE_USERNAME", "strapi"),
+          password: env("DATABASE_PASSWORD", "strapi"),
+          ssl: {
+            rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+          },
+        },
+        debug: false,
+      },
+    };
+  }
+
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: "sqlite",
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
